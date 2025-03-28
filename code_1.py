8db6bed0ca1eb31a84b7e329bbc72985fcde3803
Cihan Keles <cihan.keles@yeditepe.edu.tr>
2025-03-28 17:17:36 +0300

    code file was changed

--- code_1.py
+++ code_1.py
@@ -2,3 +2,14 @@
 print("Hello world!")
 # New line is added
 print("Hey! I'm changing the file!")
+
+# New line is added
+print("Hey! I'm changing the file!")
+
+
+for i in range(5,-1,-1):
+    print("--->", i)
+    time.sleep(1)
+
+
+print("Rocket was launched!")
